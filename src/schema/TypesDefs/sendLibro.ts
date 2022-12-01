27f7a5502b1e2151d1b71668162adaa582695b69
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-01 20:33:11 -0300

    schema de libro cambiado ahora el numero de pagina, y el total de las paginas estan fuera de results

--- src/schema/TypesDefs/sendLibro.ts
+++ src/schema/TypesDefs/sendLibro.ts
@@ -1,7 +1,5 @@
-import { GraphQLInt, GraphQLList, GraphQLObjectType } from "graphql";
+import { GraphQLBoolean, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
 import { TLibro } from "./libro";
-import { TSend } from "./send";
-
 
 export const jSendLibro = () =>
 {
@@ -9,6 +7,8 @@ export const jSendLibro = () =>
         message: "",
         success: false,
         status: 0,
+        page: 0,
+        totalPage: 0,
         results: {
             libro: new Array
         }
@@ -19,10 +19,21 @@ const ObjectLibro = new GraphQLObjectType({
     name: `ObjectLibro`,
     fields: 
     {
-        page: {type: GraphQLInt},
-        totalPage: {type: GraphQLInt},
         libro: {type: new GraphQLList(TLibro)}
     }
 })
+
+const TSend = (name: string, TypeDef: GraphQLObjectType) => new GraphQLObjectType({
+    name: `send_${name}`,
+    fields: 
+    {
+        message: {type: GraphQLString},
+        success: {type: GraphQLBoolean},
+        status: {type: GraphQLString},
+        page: {type: GraphQLInt},
+        totalPage: {type: GraphQLInt},
+        results: {type: TypeDef}
+    }
+});
 
 export const TSendLibro = TSend('sendLibro', ObjectLibro)
