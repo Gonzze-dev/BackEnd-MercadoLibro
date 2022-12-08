1201b7f26e270a5f02b546291017cc707ba451ff
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-08 01:01:23 -0300

    MP token cambiado

--- src/config.ts
+++ src/config.ts
@@ -10,4 +10,4 @@ export const DB_PORT = process.env.DB_PORT || '5432';
 export const DB_NAME = process.env.DB_NAME || 'test';
 
 export const JWT_SECRET = process.env.JWT_SECRET || "4EDDE5877A29AC000";
-export const MERCADO_PAGO_TOKEN = process.env.MERCADO_PAGO_TOKEN || "TEST-102529111039618-111319-be2cad25aff0465082c188157129480d-184613295";
+export const MERCADO_PAGO_TOKEN = process.env.MERCADO_PAGO_TOKEN || "TEST-3730969068296712-120721-592bdc32fe10fcd9073ed2d4f3f1a661-1257958452";
