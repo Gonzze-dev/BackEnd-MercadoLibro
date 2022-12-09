c1a36a09959b9ceda832740d1e499bcaf526f6f8
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-09 01:00:23 -0300

    linea del carrito ahora tiene cupon

--- src/Entities/Linea_carrito.ts
+++ src/Entities/Linea_carrito.ts
@@ -6,6 +6,7 @@ import { BaseEntity,
         JoinColumn, 
         ManyToOne, 
         PrimaryGeneratedColumn } from "typeorm"
+import { Cupon } from "./Cupon";
 import { Libro } from "./Libro"
 import { Usuario } from "./Usuario"
 
@@ -20,6 +21,15 @@ export class Linea_carrito extends BaseEntity
     @Field(type => Int)
     @Column()
     cantidad!: number;
+
+    @Field(type => Cupon, {nullable: true})
+    @ManyToOne((type) => Cupon, (Cupon) => Cupon.codigo_cupon, {
+        onUpdate: 'CASCADE',
+        eager: true
+    })
+    @JoinColumn({name: 'codigo_cupon'})
+    cupon: Cupon;
+
     
     @Field(type => Libro, {nullable: true})
     @ManyToOne((type) => Libro, (libro) => libro.isbn, {
