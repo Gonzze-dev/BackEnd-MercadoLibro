42db55c3ed6e7fc5def6e09e86fb27b82add9387
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-09 01:14:49 -0300

    argsCupon ruta eliminada

--- src/schema/Resolvers/Cupon/CuponResolver.ts
+++ src/schema/Resolvers/Cupon/CuponResolver.ts
@@ -4,7 +4,7 @@ import { Resolver, Query} from "type-graphql";
 import { SendCupon } from "../../../SendTypes/SendCupon";
 import { AgregarCupon } from "../../Mutations/Usuario/agregarCupon";
 import { GetTemas } from "../../Queries/Tema/getTemas";
-import { ArgsAgregarCupon } from "./argsDefs";
+
 
 @Resolver()
 export class CuponResolver
