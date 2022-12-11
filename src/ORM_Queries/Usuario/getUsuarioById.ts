3335d01bbd192fc5a83ac303017daf9ab41aaa6d
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-11 03:52:40 -0300

    prueba de rendimiento 2

--- src/ORM_Queries/Usuario/getUsuarioById.ts
+++ src/ORM_Queries/Usuario/getUsuarioById.ts
@@ -4,6 +4,22 @@ export async function getUsuarioById(id: number)
 {
     const usuario = await Usuario.find(
         {
+            relations:
+            {
+                orden:
+                {
+                    direccion_entrega: true,
+                    cupon: true,
+                    orden_detalle:{
+                        libro: true
+                    }
+                },
+                favorito: true,
+                carrito: {
+                    libro: true
+                },
+                direccion: true
+            },
             where:
             {
                 id: id
