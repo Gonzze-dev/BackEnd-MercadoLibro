6daa4a578dfc24393e6ed9ce8ba9c987ad9be3c7
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-11 20:44:56 -0300

    libreria senduer borrada

--- src/schema/Mutations/Usuario/insertFav.ts
+++ src/schema/Mutations/Usuario/insertFav.ts
@@ -3,7 +3,6 @@ import { verify } from "jsonwebtoken";
 import { JWT_SECRET } from "../../../config"
 import { insertFav } from "../../../ORM_Queries/Usuario/insertFav";
 import { Send } from "../../../SendTypes/Send";
-import { SendUsuario } from "../../../SendTypes/SendUsuario";
 
 export async function InsertFav(isbn: string, tokenUser: string) {
 	const msj = new Send()
