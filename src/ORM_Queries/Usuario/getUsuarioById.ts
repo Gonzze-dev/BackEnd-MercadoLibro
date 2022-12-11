e3b0122a72b82839fc634c508f2901c78f6c5821
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-11 04:07:30 -0300

    prueba 7 rendimiento

--- src/ORM_Queries/Usuario/getUsuarioById.ts
+++ src/ORM_Queries/Usuario/getUsuarioById.ts
@@ -18,6 +18,7 @@ export async function getUsuarioById(id: number)
                         libro: true
                     }
                 },
+                direccion: true
             },
             where:
             {
