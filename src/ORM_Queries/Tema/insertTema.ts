747382ec3d769893cdbe43328fd6288483d8bb5e
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-12 18:45:25 -0300

    insert tema como mutacion agregado

--- src/ORM_Queries/Tema/insertTema.ts
+++ src/ORM_Queries/Tema/insertTema.ts
@@ -1,21 +1,20 @@
 import { Tema } from "../../Entities/Tema";
 import { existsName } from "../Utilities/exists";
-import { getElementByName } from "../Utilities/getElementByName";
 
 export async function insertTema(tema: any)
 {
     const existeTema = await existsName(tema, Tema);
+    
+
+    if (existeTema)
+    {
+        throw `ERROR, EL TEMA ${tema} YA EXISTE`
+    }
 
     const obj_tema = new Tema();
 
-    // if (!existeTema)
-    // {
-    //     obj_tema.nombre = tema.nombre;
-    //     obj_tema.url_imagen = tema.url_imagen;
+    obj_tema.nombre = tema.nombre;
+    obj_tema.url_imagen = tema.url_imagen;
 
-    //     await obj_tema.save()
-    // }
-
-
-    return await getElementByName(tema, Tema)
+    await obj_tema.save()
 }
--- src/schema/Resolvers/Tema/TemaResolver.ts
+++ src/schema/Resolvers/Tema/TemaResolver.ts
@@ -1,5 +1,7 @@
-import { Resolver, Query} from "type-graphql";
+import { Resolver, Query, Mutation, Arg} from "type-graphql";
+import { Send } from "../../../SendTypes/Send";
 import { SendTema } from "../../../SendTypes/SendTema";
+import { InsertTema } from "../../Mutations/Tema/insertTema";
 import { GetTemas } from "../../Queries/Tema/getTemas";
 
 @Resolver()
@@ -9,6 +11,12 @@ export class TemaResolver
     async getTemas()
     {
         return await GetTemas();
+    }
+
+    @Mutation(() => Send)
+    async insertTema(@Arg('tema') tema: string)
+    {
+        return await InsertTema(tema);
     }
 
 }
