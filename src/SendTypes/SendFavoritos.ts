19233dcbddd822d1f7f9dc60b4f4803c68bbae86
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-12 15:27:25 -0300

    implementado para enviar favoritos del usuario

---
+++ src/SendTypes/SendFavoritos.ts
@@ -0,0 +1,19 @@
+
+import { Field, Int, ObjectType } from "type-graphql";
+import { Libro } from "../Entities/Libro";
+
+@ObjectType()
+export class SendFavoritos
+{
+    @Field()
+    message: string = "";
+
+    @Field()
+    success:boolean = false;
+
+    @Field(type => Int)
+    status: number = 0;
+
+    @Field(type => [Libro], {nullable: true})
+    favorito: Libro[];
+}
