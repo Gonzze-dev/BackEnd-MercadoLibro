950d51a92ace9556efae81863528feac5c53ab44
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-13 15:42:55 -0300

    resoplvers pais, provincia y ciudad implementado

--- src/server.ts
+++ src/server.ts
@@ -10,60 +10,66 @@ import { MERCADO_PAGO_TOKEN } from "./config";
 import { crearOrden } from "./ORM_Queries/Orden/crearOrden";
 import { CuponResolver } from "./schema/Resolvers/Cupon/CuponResolver";
 import { OrdenRsolver } from "./schema/Resolvers/Orden/OrdenResolver";
+import { CiudadResolver } from "./schema/Resolvers/Ciduad/CiudadResolver";
+import { PaisResolver } from "./schema/Resolvers/Pais/PaisResolver";
+import { ProvinciaResolver } from "./schema/Resolvers/Provincia/ProvinciaResolver";
 
 const mercadopago = require("mercadopago")
 
 export async function startServer() {
 
-  const app = express();
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({extended: true}))
+    const app = express();
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
 
-  const server = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [LibroResolver, 
-                  UsuarioResolver, 
-                  TemaResolver, 
-                  CuponResolver,
-                  OrdenRsolver],
-      validate: false
-    }),
-    context: ({ req, res }) => ({ req, res })
-  });
+    const server = new ApolloServer({
+        schema: await buildSchema({
+          resolvers: [LibroResolver, 
+                      UsuarioResolver, 
+                      TemaResolver, 
+                      CuponResolver,
+                      OrdenRsolver,
+                      CiudadResolver,
+                      PaisResolver, 
+                      ProvinciaResolver],
+        validate: false
+        }),
+        context: ({ req, res }) => ({ req, res })
+    });
 
-  await server.start()
+    await server.start()
 
-  app.post('/notificar', async (req: any, res: any) => {
-      mercadopago.configure({access_token: MERCADO_PAGO_TOKEN});
+    app.post('/notificar', async (req: any, res: any) => {
+        mercadopago.configure({access_token: MERCADO_PAGO_TOKEN});
 
-      const {query} = req
+        const {query} = req
 
-      let payment: any
+        let payment: any
 
-      const topic = query.topic || query.type;
-      
-  
-      if (topic == "payment") {
+        const topic = query.topic || query.type;
+        
+    
+        if (topic == "payment") {
 
-          const paymentId = query.id || query['data.id'];
-          payment = await mercadopago.payment.findById(paymentId)
-          
-          const items = payment.body.additional_info.items
-          const status = payment.body.status
+            const paymentId = query.id || query['data.id'];
+            payment = await mercadopago.payment.findById(paymentId)
+            
+            const items = payment.body.additional_info.items
+            const status = payment.body.status
 
-          await crearOrden(status, items, <string>paymentId)
-          
-          res.status(200)
-      }
-      else
-      {
-        res.status(400)
-      }
+            await crearOrden(status, items, <string>paymentId)
+            
+            res.status(200)
+        }
+        else
+        {
+          res.status(400)
+        }
 
-  })
+    })
 
 
-  server.applyMiddleware({ app, path: "/graphql" });
+    server.applyMiddleware({ app, path: "/graphql" });
 
-  return app;
+    return app;
 }
