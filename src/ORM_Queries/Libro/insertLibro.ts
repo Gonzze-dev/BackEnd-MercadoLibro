edd52ae4d68cef1c42134f8cbe666c2f5db97af4
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-13 00:13:43 -0300

    fecha_ingreso console logs para prueba

--- src/ORM_Queries/Libro/insertLibro.ts
+++ src/ORM_Queries/Libro/insertLibro.ts
@@ -33,7 +33,19 @@ export async function insertLibro(isbn: string,
     obj_libro.precio = precio;
     obj_libro.stock = stock;
     obj_libro.descripcion = descripcion;
-
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
+    console.log(fecha_ingreso)
     if (fecha_ingreso.length > 0)
     {
         obj_libro.fecha_ingreso = fecha_ingreso
