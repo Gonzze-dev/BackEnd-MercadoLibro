1e679291c03e1b5ef68aea803a94b3655c9d2905
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-13 20:39:27 -0300

    eliminar tema corregido

--- src/ORM_Queries/Tema/eliminarTema.ts
+++ src/ORM_Queries/Tema/eliminarTema.ts
@@ -6,8 +6,9 @@ export async function eliminarTema(nombre: string)
 {
     const tema = await getElementByName(nombre, Tema);
 
-    if(!tema[0])
+    if(tema[0])
         throw `ERROR, EL TEMA NO EXISTE`
 
+    
     await tema[0].remove()
 }
