ed65d26a6f34504740e98ab530157c987d44bf95
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-13 00:44:54 -0300

    fecha ingreso arreglada

--- src/ORM_Queries/Utilities/formatedDate.ts
+++ src/ORM_Queries/Utilities/formatedDate.ts
@@ -1,7 +1,11 @@
-export const formatedDate = (date: any) => {
+function padTo2Digits(num: any) {
+    return num.toString().padStart(2, '0');
+}
+
+export function formatedDate(date: any) {
     return [
-        date.getDate(),
-        (date.getMonth() + 1),
-        date.getFullYear()
-        ].join('/')
+        padTo2Digits(date.getDate()),
+        padTo2Digits(date.getMonth() + 1),
+        date.getFullYear(),
+    ].join('/');
 }
