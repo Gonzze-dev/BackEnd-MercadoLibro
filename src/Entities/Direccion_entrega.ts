b0d3638a162b11bca2b9a673b817059ed826883b
gonzalo errandonea <errandoneagonzalo18@gmail.com>
2022-12-17 22:42:34 -0300

    hereda de Direccion ahora, hace esepjo de la misma

--- src/Entities/Direccion_entrega.ts
+++ src/Entities/Direccion_entrega.ts
@@ -1,60 +1,12 @@
-import { Field, ID, Int, ObjectType } from "type-graphql";
-import { BaseEntity, 
-        Column,
-        Entity,
-        JoinColumn,
-        ManyToOne,
-        PrimaryGeneratedColumn} from "typeorm";
+import {ObjectType } from "type-graphql";
+import { Entity } from "typeorm";
 
-import { Ciudad } from "./Ciudad";
-    
-import { Usuario } from "./Usuario";
+import { Direccion } from "./Direccion";
+
 
 @ObjectType()
 @Entity()
-export class Direccion_entrega extends BaseEntity
+export class Direccion_entrega extends Direccion
 {
-    @Field(type => ID)
-    @PrimaryGeneratedColumn()
-    id!: number;
 
-    @Field({nullable: true})
-    @Column({nullable: true})
-    nombre!: string;
-
-    @Field()
-    @Column()
-    direccion!: string;
-
-    @Field()
-    @Column({
-        nullable: true
-    })
-    infoAdicional: string;
-
-    @Field(type => Int, {nullable: true})
-    @Column({type: 'bigint'})
-    dni!: number;
-
-    @Field({
-        nullable: true
-    })
-    @Column({
-        nullable: true
-    })
-    telefono: string;
-
-    @ManyToOne((type) => Usuario, {
-        onUpdate: 'CASCADE'
-    })
-    @JoinColumn({name: 'id_usuario'})
-    usuario!: Usuario;
-
-    @Field(type => Ciudad)
-    @ManyToOne((type) => Ciudad, {
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-    })
-    @JoinColumn({name: 'cp'})
-    ciudad!: Ciudad;
 }
